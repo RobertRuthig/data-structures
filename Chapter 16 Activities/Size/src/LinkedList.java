6cfcf6fec5b18aa112ea0050ce3fb8d65c1a24ca
Robert Ruthig <rhruthig@stu.naperville203.org>
2024-09-30 13:43:06 -0500

    skibidi

--- Chapter 16 Activities/Size/src/LinkedList.java
+++ Chapter 16 Activities/Size/src/LinkedList.java
@@ -24,7 +24,14 @@ public class LinkedList
     */
     public int size()
     {
-        . . .
+        int count = 0;
+        ListIterator it = this.listIterator();
+        while(it.hasNext())
+        {
+            count++;
+            it.next();
+        }
+        return count;
     }
 
     /**
